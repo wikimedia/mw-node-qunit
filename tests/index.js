8c9649e87837d6bd664084687feeef33e3c13479
joakin <joaquin@chimeces.com>
2017-05-23 19:46:49 +0200

    Add a simple test that touches the DOM

--- tests/index.js
+++ tests/index.js
@@ -1,32 +1,53 @@
 QUnit.module('Globals');
 
-QUnit.test('mediaWiki global is defined', function (assert) {
-  assert.equal(typeof mediaWiki, 'object')
+QUnit.test('mediaWiki global is defined', function(assert) {
+  assert.equal(typeof mediaWiki, 'object');
 });
-QUnit.test('QUnit global is defined', function (assert) {
-  assert.equal(typeof QUnit, 'object')
+QUnit.test('QUnit global is defined', function(assert) {
+  assert.equal(typeof QUnit, 'object');
 });
-QUnit.test('window global is defined', function (assert) {
-  assert.equal(typeof window, 'object')
+QUnit.test('window global is defined', function(assert) {
+  assert.equal(typeof window, 'object');
 });
-QUnit.test('document global is defined', function (assert) {
-  assert.equal(typeof document, 'object')
+QUnit.test('document global is defined', function(assert) {
+  assert.equal(typeof document, 'object');
 });
-QUnit.test('jQuery global is defined', function (assert) {
-  assert.equal(typeof jQuery, 'function')
+QUnit.test('jQuery global is defined', function(assert) {
+  assert.equal(typeof jQuery, 'function');
 });
 
 QUnit.module('QUnit patching', {
-  setup () {
+  setup() {
     this.setup = true;
-  },
+  }
 });
 
-QUnit.test('setup and teardown methods are patched to work', function (assert) {
+QUnit.test('setup and teardown methods are patched to work', function(assert) {
   assert.equal(this.setup, true, 'setup hook was run');
 });
-QUnit.test('a sinon sandbox is added to this.sandbox', function (assert) {
+QUnit.test('a sinon sandbox is added to this.sandbox', function(assert) {
   assert.equal(typeof this.sandbox, 'object', 'sandbox is created');
-  assert.equal(typeof this.sandbox.spy, 'function', 'sandbox methods are present');
-  assert.equal(typeof this.sandbox.stub, 'function', 'sandbox methods are present');
+  assert.equal(
+    typeof this.sandbox.spy,
+    'function',
+    'sandbox methods are present'
+  );
+  assert.equal(
+    typeof this.sandbox.stub,
+    'function',
+    'sandbox methods are present'
+  );
+});
+
+QUnit.module('A test that uses the DOM', {
+  beforeEach() {
+    this.el = $('<div id="test"/>').text('banana').append(window.document.body);
+  },
+  afterEach() {
+    this.el.remove();
+  }
+});
+
+QUnit.test('setting an element on the DOM works', function(assert) {
+  assert.equal(this.el.text(), 'banana', 'text was set on the dom properly');
 });
